6ad4fd677eac269db718b7fab6385742b24b464e
Himel Roy <freelancer.himelroy@gmail.com>
2022-12-30 05:45:13 +0600

    admin auth added

---
+++ pages/Admin/login.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import Login from "../../components/Admin/Login/Login";
+
+export default function login() {
+  return (
+    <div>
+      <Login />
+    </div>
+  );
+}
---
+++ pages/Admin/register.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import Register from "../../components/Admin/Register/Register";
+
+export default function register() {
+  return (
+    <div>
+      <Register />
+    </div>
+  );
+}
